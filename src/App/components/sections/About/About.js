762083373a05b3152366b955d3403a31c0cd88b5
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-05-10 03:19:52 -0700

    Add About section

--- src/App/components/sections/About/About.js
+++ src/App/components/sections/About/About.js
@@ -3,7 +3,26 @@ import React from 'react';
 const About = () => {
 	return (
 		<div className="about">
-			About
+			<div className="about-content-container">
+				<div className="about-left">
+					<h2>About</h2>
+					<div className="about-img"></div>
+				</div>
+				<div className="about-right">
+					<h3>Who am I?</h3>
+					<p className="about-title">Full Stack Developer</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur, adipiscing elit, sed
+						do eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+						ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+						cupidatat non proident, sunt in culpa qui officia deserunt
+						mollit anim id est laborum.
+						</p>
+				</div>
+			</div>
 		</div>
 	);
 }
