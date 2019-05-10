24fc08a6563f0331c0061e31ab371f988bdc15d6
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-05-10 03:19:28 -0700

    Create Home section

--- src/App/components/sections/Home/Home.js
+++ src/App/components/sections/Home/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+//images
+import homeImage from './assets/home-image.png';
+
 const Home = () => {
 	return (
 		<div className="home">
-			
+			<div className="home-content-container">
+				<div className="home-content-left">
+					<h1 className="home-name">Tony Nguyen</h1>
+					<div className="home-quote-container">
+						<p className="home-quote-marks">"</p>
+						<p className="home-quote">
+							Lorem ipsum dolor sit amet, 
+							consectetur adipiscing elit, sed do 
+							eiusmod tempor incididunt ut labore
+							et dolore magna aliqua.
+						</p>
+						<div className="home-author-container">
+							<div className="home-line"></div>
+							<p>
+								Person Name
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="home-content-right">
+					<img src={homeImage} alt="people studying"/>
+				</div>
+			</div>
 		</div>
 	);
 }
