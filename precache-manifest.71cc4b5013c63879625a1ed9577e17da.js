ab564073f543a076edcda9ac376e0475b45ed192
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-05-20 19:14:46 -0700

    Updates

---
+++ precache-manifest.71cc4b5013c63879625a1ed9577e17da.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a5c6a2d892445c59287b823aee8eba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5dd68357b74e4299d0f9",
+    "url": "/portfolio/static/css/main.cf75d577.chunk.css"
+  },
+  {
+    "revision": "574f4b7b6ef6af4307a8",
+    "url": "/portfolio/static/js/2.e7752337.chunk.js"
+  },
+  {
+    "revision": "5dd68357b74e4299d0f9",
+    "url": "/portfolio/static/js/main.5d52329b.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "5953d59c0573371461d584967e304db3",
+    "url": "/portfolio/static/media/arrow.5953d59c.svg"
+  },
+  {
+    "revision": "6ef364c0c36b46edde16c816da8d020b",
+    "url": "/portfolio/static/media/big-js-icon.6ef364c0.png"
+  },
+  {
+    "revision": "bd80bfae55e58ca2a1eac104c6ef72e6",
+    "url": "/portfolio/static/media/blue-bg.bd80bfae.png"
+  },
+  {
+    "revision": "9a1c45e1e7aaf1cab05208fc17fcd42f",
+    "url": "/portfolio/static/media/blue-exit.9a1c45e1.svg"
+  },
+  {
+    "revision": "8ad02706e3e1991efd045d79927475d9",
+    "url": "/portfolio/static/media/home-image.8ad02706.png"
+  },
+  {
+    "revision": "c8ad680ca87cb02c3a16a99e0c6e3492",
+    "url": "/portfolio/static/media/laptop.c8ad680c.png"
+  },
+  {
+    "revision": "3df206c6f078e258a1b017caca677e60",
+    "url": "/portfolio/static/media/numbers.3df206c6.jpg"
+  },
+  {
+    "revision": "9370bc002b683dc95b126fd15e1bfc51",
+    "url": "/portfolio/static/media/phone.9370bc00.png"
+  },
+  {
+    "revision": "e9f2c5b13dd460304bdce5d8daa8eed6",
+    "url": "/portfolio/static/media/phones.e9f2c5b1.jpg"
+  },
+  {
+    "revision": "bae0584a37510368e6959bef0d9e6ad3",
+    "url": "/portfolio/static/media/self-image.bae0584a.jpg"
+  },
+  {
+    "revision": "5b27ce01b6251448475c169921850a8e",
+    "url": "/portfolio/static/media/stats.5b27ce01.jpg"
+  },
+  {
+    "revision": "7ad23076290c2f8f513245b85a9f5ed5",
+    "url": "/portfolio/static/media/tablet.7ad23076.jpg"
+  },
+  {
+    "revision": "f0d6120ac7e826602cefd5ffdb68a7de",
+    "url": "/portfolio/static/media/take-stand-large.f0d6120a.png"
+  },
+  {
+    "revision": "b02a0b5be1787c3eb23c21323c182bf0",
+    "url": "/portfolio/static/media/take-stand.b02a0b5b.jpg"
+  },
+  {
+    "revision": "cfafab7ab78a8f60b8de81867b31e5b9",
+    "url": "/portfolio/static/media/two-phones.cfafab7a.jpg"
+  },
+  {
+    "revision": "81996afc87580aa805b867f0cf0a84be",
+    "url": "/portfolio/static/media/white-exit.81996afc.svg"
+  }
+]);
