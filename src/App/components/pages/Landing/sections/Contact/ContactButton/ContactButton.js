a4b05e4a6b7b4b70b5ec30b5034ac7609cec512e
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-07-06 01:22:50 -0700

    Make contact buttons open new tab

--- src/App/components/pages/Landing/sections/Contact/ContactButton/ContactButton.js
+++ src/App/components/pages/Landing/sections/Contact/ContactButton/ContactButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactButton = (props) => {
 	const { url, src, alt } = props;
 	return (
-		<a href={url} className="contact-button">
+		<a href={url} className="contact-button" target="_blank">
 			<img src={src} alt={alt} />
 		</a>
 	);
